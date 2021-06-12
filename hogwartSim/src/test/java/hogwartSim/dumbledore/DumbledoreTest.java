bce77407dc3465d130c1193e27b5c4185f1a881f
dominikkilijan <dominikkilijan@gmail.com>
2021-06-13 00:18:20 +0200

    Test begining

---
+++ hogwartSim/src/test/java/hogwartSim/dumbledore/DumbledoreTest.java
@@ -0,0 +1,16 @@
+package hogwartSim.dumbledore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DumbledoreTest {
+
+    @Test
+    void addPoints() {
+    }
+
+    @Test
+    void declareWinner() {
+    }
+}
