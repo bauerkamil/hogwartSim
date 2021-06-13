353af55cfc3e35619ed9e7d22a167d81458e8910
Kamil Bauer <259102@student.pwr.edu.pl>
2021-06-14 00:52:02 +0200

    got back tests

--- hogwartSim/src/test/java/hogwartSim/dumbledore/DumbledoreTest.java
+++ hogwartSim/src/test/java/hogwartSim/dumbledore/DumbledoreTest.java
@@ -15,12 +15,34 @@ class DumbledoreTest {
         //when
         dumbledore.addPoints(HogwartHouses.GRYFFINDOR, 50);
         //then
-        Assertions.assertEquals(50, dumbledore.housesPoints[0]);
+        Assertions.assertEquals(50, dumbledore.housesPoints[HogwartHouses.GRYFFINDOR.getValue()]);
     }
 
-    /*@Test
-    void declareWinner() {
-        //given
-        Dumbledore dumbledore = new Dumbledore();
-    }*/
+//    @Test
+//    void declareWinnerWhenSlytherinHasTheMostPoints() {
+//        //given
+//        Dumbledore dumbledore = new Dumbledore();
+//        dumbledore.housesPoints = new int[]{0, 100, 0, 0};
+//        //then
+//        Assertions.assertEquals( dumbledore.declareWinner(), "Slytherin");
+//
+//    }
+//    @Test
+//    void declareWinnerToTheFirstInListIfMultipleHousesHaveTheSamePoints() {
+//        //given
+//        Dumbledore dumbledore = new Dumbledore();
+//        dumbledore.housesPoints = new int[]{50, 100, 0, 100}; //Dumbledore adds 50 points to Gryffindor when declaring the Winner
+//        //then
+//        Assertions.assertEquals( dumbledore.declareWinner(), "Gryffindor");
+//
+//    }
+//    @Test
+//    void declareWinnerWhenNumbersAreNegative() {
+//        //given
+//        Dumbledore dumbledore = new Dumbledore();
+//        dumbledore.housesPoints = new int[]{-100,-100, -10, -200};
+//        //then
+//        Assertions.assertEquals( dumbledore.declareWinner(), "Hufflepuff");
+//
+//    }
 }
